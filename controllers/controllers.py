1e67fda6e22ece2dd11602a7b4488a8c36c07eee
Mizara983 <pounasatu@gmail.com>
2023-02-03 16:53:53 +0300

    Correction API product

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
+import json
 
 class IndexCustomApi(http.Controller):
     @http.route('/index_custom_api/product/api/', auth='public', website=False, csrf=False, type="json", methods=['GET', 'POST'])
@@ -8,22 +9,41 @@ class IndexCustomApi(http.Controller):
         # Code bar: code_bar (barcode)
         # Nom de l'article: product_name (name)
         #Cout du produit: product_cost (standard_price)
-        if(kw.get('code_bar') and kw.get('product_name') and kw.get('product_cost')):
-            code_bar = str(kw.get('code_bar'))
-            product_name = kw.get('product_name')
-            product_cost = kw.get('product_cost')
-            values={
-                "name": product_name,
-                "standard_price": product_cost,
-            }
-            product_template = request.env['product.template'].sudo()
-            existing_prod = product_template.search([('barcode', '=', code_bar)])
-            if existing_prod:
-                print('*'*80)
-                print('updating record')
-                existing_prod.write(values)
+        # if(kw.get('code_bar') and kw.get('product_name') and kw.get('product_cost')):
+        #     code_bar = str(kw.get('code_bar'))
+        #     product_name = kw.get('product_name')
+        #     product_cost = kw.get('product_cost')
+        #     values={
+        #         "name": product_name,
+        #         "standard_price": product_cost,
+        #     }
+        #     product_template = request.env['product.template'].sudo()
+        #     existing_prod = product_template.search([('barcode', '=', code_bar)])
+        #     if existing_prod:
+        #         print('*'*80)
+        #         print('updating record')
+        #         existing_prod.write(values)
 
-            else:
-                print('*'*80)
-                print('No bar code found')
-        return kw
+        #     else:
+        #         print('*'*80)
+        #         print('No bar code found')
+        return 'Salut'
+
+    @http.route('/index_custom_api/product/req235/', auth='public', website=False, csrf=False, type="json", methods=['GET', 'POST'])
+    def _index_api(self, **kw):
+        products = request.env['product.template'].sudo().search([('type','=', 'product'), ('to_send', '=', True)])
+        data = []
+        for product in products:
+            product.write({"to_send": False})
+            qty_tosend = (product.qty_available * product.seuil)/100
+            values = {
+                'product_name': product.name,
+                'product_display_name': product.display_name,
+                'product_cost': product.standard_price,
+                'product_barcode': product.barcode,
+                'quantity': qty_tosend,
+            }
+            data.append(values)
+
+        json_data = json.dumps(data)
+        return json_data
--- models/models.py
+++ models/models.py
@@ -9,22 +9,30 @@ import json
 class productTemplate(models.Model):
     _inherit = "product.template"
 
-    def api_send_product(self):
+    seuil = fields.Float("Seuil")
+    to_send = fields.Boolean("To send", default=True)
 
-        print('__'*80)
-        self.send_created_product()
-        print('Product successfully sent')
-        return 'salut'
+    @api.onchange('standard_price', 'name', 'barcode','qty_available')
+    def change_to_send_state(self):
+        for record in self:
+            record.to_send = True
+
+    # def api_send_product(self):
+
+    #     print('__'*80)
+    #     # self.send_created_product()
+    #     print('Product successfully sent')
+    #     return 'salut'
 
 
-    def send_created_product(self):
-        url = 'http://clicodeal.thewebsquare.com/' # # # A changer à l'url provenant du serveur PHP
-        data = {'params': {
-                                'code_bar': self.barcode,
-                                'product_name': self.name,
-                                'product_cost': self.standard_price
-                }}
-        print(data)
-        headers = {'content-type':'application/json'}
-        requests.get(url, data=json.dumps(data), headers=headers)
-        return 'var'
+    # def send_created_product(self):
+    #     url = 'http://clicodeal.thewebsquare.com/' # # # A changer à l'url provenant du serveur PHP
+    #     data = {'params': {
+    #                             'code_bar': self.barcode,
+    #                             'product_name': self.name,
+    #                             'product_cost': self.standard_price
+    #             }}
+    #     print(data)
+    #     headers = {'content-type':'application/json'}
+    #     requests.get(url, data=json.dumps(data), headers=headers)
+    #     return 'var'
