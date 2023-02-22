1f6b13c9dc78028f4f05128af86fcaf2636f0920
Mizara983 <pounasatu@gmail.com>
2023-02-22 13:27:46 +0300

    remove filter

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -31,7 +31,7 @@ class IndexCustomApi(http.Controller):
 
     @http.route('/index_custom_api/product/req235/', auth='public', website=False, csrf=False, type="json", methods=['GET', 'POST'])
     def _index_api(self, **kw):
-        products = request.env['product.template'].sudo().search([('type','=', 'product'), ('to_send', '=', True)])
+        products = request.env['product.template'].sudo().search([('type','=', 'product')])
         data = []
         for product in products:
             product.write({"to_send": False})
