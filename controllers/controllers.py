ad5edd14c5d4dd4fd728a16cedfe8542685a50a4
Mizara983 <pounasatu@gmail.com>
2023-02-24 11:44:30 +0300

    api code bar

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -31,7 +31,10 @@ class IndexCustomApi(http.Controller):
 
     @http.route('/index_custom_api/product/req235/', auth='public', website=False, csrf=False, type="json", methods=['GET', 'POST'])
     def _index_api(self, **kw):
-        products = request.env['product.template'].sudo().search([('type','=', 'product')])
+        domain = [('type','=', 'product')]
+        if kw.get('ugs'):
+            domain += [('barcode', 'in', kw.get('ugs'))]
+        products = request.env['product.template'].sudo().search(domain)
         data = []
         for product in products:
             product.write({"to_send": False})
